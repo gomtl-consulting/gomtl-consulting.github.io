03b54e224ac40943132831793b62259d907a6e03
frnkly <frank@frnk.ca>
2020-12-28 14:25:01 -0500

    Add theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,17 @@
 module.exports = {
-    plugins: [
-
-    ]
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+    },
+  ],
+  siteMetadata: {
+    siteTitle: `Gomtl.`,
+    siteTitleAlt: `Gomtl.`,
+    siteHeadline: `Gomtl.`,
+    siteUrl: `https://gomtl-consulting.github.io`,
+    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+  },
 }
